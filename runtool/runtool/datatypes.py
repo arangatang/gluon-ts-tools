bda14e2068869b8791207f38b6aedc140467ba27
Leonardo Araneda Freccero <freccero@amazon.com>
2021-01-20 11:38:58 +0100

    Added docstring for datatypes

--- runtool/runtool/datatypes.py
+++ runtool/runtool/datatypes.py
@@ -4,6 +4,20 @@ from pydantic import BaseModel
 
 
 class Versions(BaseModel):
+    """
+    The `Versions` class is used to represent an object which can
+    take several different values.
+
+    There are two ways of instantiating the `Versions` object, both require
+    a list with the different values which the `Versions` should represent.
+
+    >>> Versions.parse_obj([1, 2, 3])
+    Versions([1, 2, 3])
+
+    >>> Versions(__root__=[1, 2, 3])
+    Versions([1, 2, 3])
+    """
+
     __root__: List[Any]
 
     def __repr__(self):
@@ -20,6 +34,15 @@ class Versions(BaseModel):
 
 
 class DotDict(dict):
+    """
+    The `DotDict` class allows accessing items in a dict using a
+    dot syntax.
+
+    >>> dotdict = DotDict({"a":{"b":"hello"}})
+    >>> dotdict.a.b
+    'hello'
+    """
+
     __getattr__ = dict.__getitem__
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
