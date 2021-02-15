58b91fbec5c8e3af5d2f5ce3a2f578e909a8db8b
Leonardo Araneda Freccero <freccero@amazon.com>
2021-02-15 13:49:49 +0100

    Updated docstring for infer_types.

--- runtool/runtool/infer_types.py
+++ runtool/runtool/infer_types.py
@@ -5,15 +5,42 @@ from runtool.datatypes import Algorithm, Algorithms, Dataset, Datasets
 
 def infer_type(node: Union[list, dict]) -> Any:
     """
-    infer_type converts a list or a dict into one
-    of the following objects if the node has a matching structure.
+    infer_type converts a list or a dict into an instance
+    of one of the following classes.
 
     - Algorithm
-    - Dataset
     - Algorithms
+    - Dataset
     - Datasets
 
-    If no match is found for the node, the node is returned unaltered
+    Example:
+    >>> algorithm = {
+    ...     "image": "image_name",
+    ...     "instance": "ml.m5.2xlarge",
+    ...     "hyperparameters": {},
+    ... }
+    >>> dataset = {
+    ...     "path": {
+    ...         "train": "some path"
+    ...     },
+    ...     "meta": {},
+    ... }
+
+    >>> isinstance(infer_type(algorithm), Algorithm)
+    True
+
+    >>> isinstance(infer_type([algorithm]), Algorithms)
+    True
+
+    >>> isinstance(infer_type(dataset), Dataset)
+    True
+
+    >>> isinstance(infer_type([dataset]), Datasets)
+    True
+
+    >>> isinstance(infer_type({}), dict)
+    True
+
     """
     for class_ in (Algorithm, Algorithms, Dataset, Datasets):
         if class_.verify(node):
